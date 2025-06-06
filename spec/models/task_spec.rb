e5124bfa47205f5f87feac6535a32e43d7261d08
mkatoo <28562951+mkatoo@users.noreply.github.com>
2025-06-06 12:23:19 +0000

    モデルのテスト

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:task) { build(:task, user:) }
+
+  describe 'contentのバリデーション' do
+    it '空だと無効であること' do
+      task.content = ''
+      expect(task).not_to be_valid
+      expect(task.errors[:content]).to include("can't be blank")
+    end
+
+    it '空でないと有効であること' do
+      task.content = 'Test Task'
+      expect(task).to be_valid
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+  describe 'emailのバリデーション' do
+    it '空だと無効であること' do
+      user.email = ''
+      expect(user).not_to be_valid
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it '形式が不正だと無効であること' do
+      user.email = 'invalid_email'
+      expect(user).not_to be_valid
+      expect(user.errors[:email]).to include('is invalid')
+    end
+
+    it '形式が正しいと有効であること' do
+      user.email = 'test@example.com'
+      expect(user).to be_valid
+    end
+  end
 end
