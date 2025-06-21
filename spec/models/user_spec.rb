1588ce7aaf4c8607041dfffd1122a02ce1a9dba0
mkatoo <28562951+mkatoo@users.noreply.github.com>
2025-06-21 14:01:39 +0000

    fix

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,26 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { build(:user) }
+
+  describe 'nameのバリデーション' do
+    it '3文字未満だと無効であること' do
+      user.name = 'ab'
+      expect(user).not_to be_valid
+      expect(user.errors[:name]).to include('is too short (minimum is 3 characters)')
+    end
+
+    it '10文字を超えると無効であること' do
+      user.name = 'a' * 11
+      expect(user).not_to be_valid
+      expect(user.errors[:name]).to include('is too long (maximum is 10 characters)')
+    end
+
+    it '3文字以上10文字以下であれば有効であること' do
+      user.name = 'validname'
+      expect(user).to be_valid
+    end
+  end
+
   describe 'emailのバリデーション' do
     it '空だと無効であること' do
       user.email = ''
